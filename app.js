19e0158be76dc52abce0a99e03efbb10dba2dc63
Illensky <alexis.laroche.02240@gmail.com>
2021-12-06 16:08:05 +0100

    exercice done

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function personne (prenom, nom, age, sexe) {
+function Personne (prenom, nom, age, sexe) {
     this.prenom = prenom;
     this.nom = nom;
     this.age = age;
@@ -15,9 +15,13 @@ function personne (prenom, nom, age, sexe) {
 }
 
 const classe = [
-    new personne("Jean", "Pastissier", 27, "Homme"),
-    new personne("Toma", "Walle", 28, "Homme"),
-    new personne("Sam", "Saoule", 32, "Homme"),
-    new personne("Jeanne", "Ine", 23, "Femme"),
-    new personne("Elsa", "Roselesfleurs", 21, "Femme")
+    new Personne("Jean", "Pastissier", 27, "Homme"),
+    new Personne("Toma", "Walle", 28, "Homme"),
+    new Personne("Sam", "Saoule", 32, "Homme"),
+    new Personne("Jeanne", "Ine", 23, "Femme"),
+    new Personne("Elsa", "Roselesfleurs", 21, "Femme")
 ]
+
+for (let person of classe) {
+    document.getElementById("info").innerHTML += "Prénom : " + person.prenom + ", nom : " + person.nom + ", age : " + person.age + " sexe : " + person.sexe + "<br>"
+}
